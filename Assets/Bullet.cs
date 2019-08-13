3f9502bc7d40af8af3fde06d617eeed5811824e0
Evan Barnes <20evanjb@students.spa.edu>
2019-08-13 16:37:31 -0400

    Initial Commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int count = 0;
+    public int width;
+    public int height;
+    public GameObject tr1;
+    private float relmousex;
+    private float relmousey;
+    public float mousex;
+    public float mousey;
+    public float thetarad;
+    private int colcount;
+    public float thetadeg;
+    private bool orient;
+    private float lockPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("spawned");
+        width = Screen.width;
+        height = Screen.height;
+        mousex = Input.mousePosition.x;
+        mousey = Input.mousePosition.y;
+
+        relmousex = ((width/2 - Input.mousePosition.x));
+        relmousey = ((height/2 - Input.mousePosition.y));
+        thetarad = (Mathf.Atan((relmousex / relmousey)));
+        thetadeg = (thetarad * (180 / Mathf.PI));
+        if (relmousey > 0)
+        {
+            if (relmousex > 0)
+            {
+                //tbd quad 3
+                lockPos = 180 - thetadeg;
+                transform.Rotate(new Vector3(0, 0, lockPos));
+            }
+            else
+            {
+                //quadrent 4
+                lockPos = 180 - thetadeg;
+                transform.Rotate(new Vector3(0, 0, lockPos));
+            }
+        }
+        else
+        {
+            if (relmousex > 0)
+            {
+                //quadrent 2
+                lockPos = 0 - thetadeg;
+                transform.Rotate(new Vector3(0, 0, lockPos));
+            }
+            else
+            {
+                //quadrent 1
+                lockPos = 0 - thetadeg;
+                transform.Rotate(new Vector3(0, 0, lockPos));
+            }
+        }
+        if(colcount > 3)
+        {
+            DestroyObject(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.IgnoreCollision(tr1.transform.GetComponent<Collider>(), GetComponent<Collider>());
+        if (count > 60)
+        {
+            //Destroy(gameObject);
+        }
+        
+        transform.Translate(Vector2.up * Time.deltaTime * 4);
+    }
+    void OnCollisionStay()
+    {
+    }
+        private void OnCollisionEnter(Collision collision)
+    {
+       
+        Debug.Log("collided");
+        if (collision.gameObject.tag == "wall")
+        {
+            //Radians to Degrees
+            colcount++;
+            float f = (transform.rotation.w * (180 / Mathf.PI));
+            lockPos = 180+f;
+        
+            transform.Rotate(new Vector3(0, 0, lockPos));
+        }
+        
+            
+        
+    }
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private bool a;
+    private bool d;
+    private bool w;
+    private bool s;
+    
+    public Object prefab1;
+    public Transform tr1;
+    private Vector3 v31;
+    private Quaternion qt1;
+    private float relmousex;
+    private float relmousey;
+    private float mousex;
+    private float mousey;
+    public bool check = false;
+    public Vector4 v41;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //moveChar();
+        shoot();
+        moveChar();
+        
+        
+
+
+    }
+    void shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            v31[0] = transform.position.x;
+            v31[1] = transform.position.y;
+
+            GameObject bullet = Instantiate(prefab1, v31, qt1) as GameObject;
+            Physics.IgnoreCollision(bullet.transform.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+    //checks for key presses
+    void moveChar() {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * 4);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * 4);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * 4);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * 4);
+        }
+
+    }
+    
+}
+;
